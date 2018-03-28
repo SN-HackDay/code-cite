455e39499fd1739cce3c5539cc0959a553af43c6
Robin Long <r.long@lancs.ac.uk>
2018-03-28 13:11:29 +0100

    Updated with check_github_licence_exists function

--- notebooks/process_urls.py
+++ notebooks/process_urls.py
@@ -7,3 +7,17 @@ def is_url_valid(url):
         return True
     else:
         return False
+
+from github import Github
+
+# g = Github(token)
+# repo = g.get_repo(https://github.com/andreww/MSAT)
+
+def check_github_licence_exists(repo):
+    possible_filenames = ['LICENCE', 'COPYING','LICENSE']
+    for content in repo.get_contents('/'):
+        name = content.name
+        for poss_name in possible_filenames:
+            if poss_name in name.upper():
+                return True
+    return False
