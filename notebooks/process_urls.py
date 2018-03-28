8c83df15acd33833270134ee1ce9b3ab67365a21
Andrew Walker <a.walker@leeds.ac.uk>
2018-03-28 11:39:44 +0100

    Update process_urls.py

--- notebooks/process_urls.py
+++ notebooks/process_urls.py
@@ -4,6 +4,6 @@ def is_url_valid(url):
     response = requests.get(url)
     return_code = response.status_code
     if return_code == 200:
-        return 1
+        return True
     else:
-        return 0
+        return False
