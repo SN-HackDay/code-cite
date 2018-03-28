515a49e7e8f71b9b20df87bc7631d9f08428792b
Robin Long <r.long@lancs.ac.uk>
2018-03-28 14:28:31 +0100

    changed module for process_zenodo

--- notebooks/process_zenodo.py
+++ notebooks/process_zenodo.py
@@ -1,8 +1,8 @@
-import urllib2
+import requests
 
 def check_zenodo_licence_exists(url):
-    response = urllib2.urlopen(url)
-    content = response.read()
+    response = requests.get(url)
+    content = response.text
     if 'license' in content:
         return True
     return False
